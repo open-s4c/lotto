8df1006dda115ea348ee4923d1d76c9b0183c6c6
Diogo Behrens <db7@sdf.org>
2025-10-22 08:18:26 +0200

    Initial commit
    Signed-off-by: Diogo Behrens <db7@sdf.org>

---
+++ test/integration/mutex_test.c
@@ -0,0 +1,37 @@
+// RUN: %lotto stress -r 5 -- %b | %check
+// CHECK: [lotto-mock] subcommand=stress
+// CHECK: [lotto-mock] target=mutex_test
+// CHECK: [lotto-mock] status=ok
+
+#include <pthread.h>
+
+#define N_THREADS 4
+
+static pthread_mutex_t g_mutex = PTHREAD_MUTEX_INITIALIZER;
+
+static void *
+thread(void *arg)
+{
+    (void)arg;
+    pthread_mutex_lock(&g_mutex);
+    pthread_mutex_unlock(&g_mutex);
+    return NULL;
+}
+
+int
+main(void)
+{
+    pthread_t threads[N_THREADS];
+
+    for (int i = 0; i < N_THREADS; ++i) {
+        if (pthread_create(&threads[i], NULL, thread, NULL) != 0) {
+            return 1;
+        }
+    }
+
+    for (int i = 0; i < N_THREADS; ++i) {
+        pthread_join(threads[i], NULL);
+    }
+
+    return 0;
+}

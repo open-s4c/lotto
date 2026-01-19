979490d4b0fc1136166f429b5ecc121f42d24cc5
Diogo Behrens <diogo.behrens@huawei.com>
2026-01-19 17:44:10 +0000

    cmake: fix POST_BUILD warnings of make_blob
    Signed-off-by: Diogo Behrens <diogo.behrens@huawei.com>

--- cmake/utils.cmake
+++ cmake/utils.cmake
@@ -1,15 +1,28 @@
 macro(make_blob TARGET FILE FILE_TARGET)
-    get_filename_component(DIR ${FILE} DIRECTORY)
-    get_filename_component(NAME ${FILE} NAME)
+    set(EXCL "chibi-scheme" "")
+    if("${FILE_TARGET}" IN_LIST EXCL)
+        set(TARGET_DEPS ${FILE})
+        get_filename_component(DIR ${FILE} DIRECTORY)
+        get_filename_component(NAME ${FILE} NAME)
+    else()
+        # assume FILE_TARGET is a library target, then use $<TARGET_FILE:.> to
+        # get the file name, and use LIBRARY_OUTPUT_DIRECTORY property as
+        # working directory. Otherwise, xxd will prepend the whole path in the
+        # variable name of the encoded file.
+        set(TARGET_DEPS ${FILE_TARGET})
+        set(FILE $<TARGET_FILE:${FILE_TARGET}>)
+        get_target_property(DIR ${FILE_TARGET} LIBRARY_OUTPUT_DIRECTORY)
+        if(${DIR} STREQUAL DIR-NOTFOUND)
+            message(FATAL "Could not find OUTPUT_DIRECTORY of ${TARGET}")
+        endif()
+        get_filename_component(NAME ${FILE} NAME)
+    endif()
+
     add_custom_command(
         OUTPUT ${CMAKE_BINARY_DIR}/include/${TARGET}.h
         COMMAND ${CMAKE_COMMAND} -E make_directory ${CMAKE_BINARY_DIR}/include
-        # xxd's first argument is the input file, the second the output file.
-        # it's not possible to use `$<TARGET_FILE>`, since the input file name
-        # is relied upon in preload.c, so the command relies on the fact that
-        # TARGET is defined in the same BINARY_DIR.
         COMMAND xxd -i ${NAME} ${CMAKE_BINARY_DIR}/include/${TARGET}.h
-        DEPENDS ${FILE_TARGET} ${FILE}
+        DEPENDS ${TARGET_DEPS}
         WORKING_DIRECTORY ${DIR})
     add_custom_target(${TARGET} DEPENDS ${CMAKE_BINARY_DIR}/include/${TARGET}.h)
 endmacro()

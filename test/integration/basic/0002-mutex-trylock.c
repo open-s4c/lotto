e200d57893d207fc20f0f8bd47934f718698424b
Diogo Behrens <diogo.behrens@huawei.com>
2026-01-22 01:09:10 +0000

    test: add multiple simple integration tests
    Signed-off-by: Diogo Behrens <diogo.behrens@huawei.com>

---
+++ test/integration/basic/0002-mutex-trylock.c
@@ -0,0 +1,63 @@
+// clang-format off
+// RUN: %lotto %record --handler-deadlock disable -- %b | grep LAST_THREAD | tee %b.record
+// RUN: %lotto %replay | grep LAST_THREAD | tee %b.replay
+// RUN: diff %b.record %b.replay
+// clang-format on
+
+#include <assert.h>
+#include <inttypes.h>
+#include <pthread.h>
+#include <sched.h>
+#include <stdint.h>
+#include <stdio.h>
+
+#include <vsync/atomic.h>
+
+#define NTHREADS 4
+
+static pthread_mutex_t mutex  = PTHREAD_MUTEX_INITIALIZER;
+static volatile uint64_t last = 0;
+static vatomic32_t start      = VATOMIC_INIT(0);
+static vatomic32_t ready      = VATOMIC_INIT(0);
+
+static void *
+thread(void *arg)
+{
+    uint64_t v = (uint64_t)(uintptr_t)arg;
+
+    vatomic_inc(&ready);
+    while (!vatomic_read(&start))
+        sched_yield();
+
+    while (pthread_mutex_trylock(&mutex))
+        ;
+    last++; // just to change the value
+    last = v;
+    last++;
+    pthread_mutex_unlock(&mutex);
+    return NULL;
+}
+
+int
+main(void)
+{
+    pthread_t threads[NTHREADS];
+
+    for (int i = 0; i < NTHREADS; ++i) {
+        if (pthread_create(&threads[i], NULL, thread, (void *)(uint64_t)i))
+            return 1;
+    }
+    while (vatomic_read(&ready) != NTHREADS)
+        sched_yield();
+    vatomic_write(&start, 1);
+
+    for (int i = 0; i < NTHREADS; ++i) {
+        pthread_join(threads[i], NULL);
+    }
+
+    assert(last > 0 && last <= 100);
+
+    printf("LAST_THREAD: %" PRIu64 "\n", last);
+
+    return 0;
+}
---
+++ test/integration/basic/0004-mutex-deadlock.c
@@ -0,0 +1,45 @@
+// ALLOW_RETRIES: 10
+// RUN: (! %lotto %stress -- %b) 2>&1 | %check %s
+// CHECK: Deadlock detected!
+
+#include <pthread.h>
+#include <sched.h>
+
+static pthread_mutex_t g_mutex1 = PTHREAD_MUTEX_INITIALIZER;
+static pthread_mutex_t g_mutex2 = PTHREAD_MUTEX_INITIALIZER;
+
+static void *
+thread_a(void *arg)
+{
+    (void)arg;
+    pthread_mutex_lock(&g_mutex1);
+    sched_yield();
+    pthread_mutex_lock(&g_mutex2);
+    pthread_mutex_unlock(&g_mutex2);
+    pthread_mutex_unlock(&g_mutex1);
+    return NULL;
+}
+
+static void *
+thread_b(void *arg)
+{
+    (void)arg;
+    pthread_mutex_lock(&g_mutex2);
+    sched_yield();
+    pthread_mutex_lock(&g_mutex1);
+    pthread_mutex_unlock(&g_mutex1);
+    pthread_mutex_unlock(&g_mutex2);
+    return NULL;
+}
+
+int
+main(void)
+{
+    pthread_t a;
+    pthread_t b;
+    pthread_create(&a, NULL, thread_a, NULL);
+    pthread_create(&b, NULL, thread_b, NULL);
+    pthread_join(a, NULL);
+    pthread_join(b, NULL);
+    return 0;
+}

268dd3e940cad7fec2296b21741cb9bf1d30dc9d
Diogo Behrens <diogo.behrens@huawei.com>
2026-01-30 11:02:19 +0000

    refactor: make handlers become modules
    Signed-off-by: Diogo Behrens <diogo.behrens@huawei.com>

--- src/interceptors/intercept_pthread_create.c
+++ src/interceptors/intercept_pthread_create.c
@@ -17,6 +17,24 @@
 #include <lotto/runtime/intercept.h>
 #include <lotto/sys/logger.h>
 
+#if 0
+int detachstate;
+if (attr == NULL) {
+    detachstate = PTHREAD_CREATE_JOINABLE;
+} else {
+    ENSURE(pthread_attr_getdetachstate(attr, &detachstate) == 0);
+}
+(void)intercept_capture(ctx(.func = __FUNCTION__, .cat = CAT_TASK_INIT,
+                            .args = {arg(uint64_t, pthread_self()),
+                                     arg(bool, detachstate ==
+                                                   PTHREAD_CREATE_DETACHED)}));
+void *ret = _run(_arg);
+return ret;
+}
+
+#endif
+
+
 // -----------------------------------------------------------------------------
 // thread_start and thread_exit
 // -----------------------------------------------------------------------------
@@ -65,7 +83,8 @@ PS_SUBSCRIBE(CAPTURE_AFTER, EVENT_THREAD_CREATE, {
 PS_SUBSCRIBE(CAPTURE_BEFORE, EVENT_THREAD_JOIN, {
     struct pthread_join_event *ev = EVENT_PAYLOAD(event);
 
-    ev->ret      = EINTR;
+    ev->ret = EINTR;
+    // alternative is CAT_JOIN
     context_t *c = ctx(.func = "pthread_join", .cat = CAT_CALL,
                        .args = {
                            [0] = arg_ptr(&ev->thread),
@@ -73,10 +92,51 @@ PS_SUBSCRIBE(CAPTURE_BEFORE, EVENT_THREAD_JOIN, {
                            [2] = arg_ptr(&ev->ret),
                        });
     (void)intercept_before_call(c);
-    return PS_OK;
 })
 
 PS_SUBSCRIBE(CAPTURE_AFTER, EVENT_THREAD_JOIN, {
     intercept_after_call("pthread_join");
     return PS_OK;
 })
+
+
+struct pthread_detach_event {
+    const void *pc;
+    pthread_t thread;
+    void (*func)(pthread_t);
+    int ret;
+};
+
+#define EVENT_PTHREAD_DETACH 7
+PS_ADVERTISE_TYPE(EVENT_PTHREAD_DETACH)
+
+PS_SUBSCRIBE(CAPTURE_BEFORE, EVENT_PTHREAD_DETACH, {
+    struct pthread_detach_event *ev = EVENT_PAYLOAD(event);
+
+    int ret = EINTR;
+    intercept_capture(ctx(.func = __FUNCTION__, .cat = CAT_DETACH,
+                          .args = {
+                              [0] = arg(uint64_t, ev->thread),
+                              [1] = arg_ptr(&ret),
+                          }));
+    ASSERT(ret != EINTR);
+    return PS_OK;
+})
+
+#if 0
+INTERPOSE(int, pthread_detach, pthread_t thread)
+{
+    struct pthread_detach_event ev = {
+        .pc     = INTERPOSE_PC,
+        .thread = thread,
+        .func   = REAL_FUNC(pthread_detach),
+        .ret    = 0,
+    };
+
+    struct metadata md = {0};
+    PS_PUBLISH(INTERCEPT_BEFORE, EVENT_PTHREAD_DETACH, &ev, &md);
+    ev.ret = ev.func(ev.thread);
+    PS_PUBLISH(INTERCEPT_AFTER, EVENT_PTHREAD_DETACH, &ev, &md);
+    return ev.ret;
+}
+#endif

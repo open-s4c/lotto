5d4d484d588a6ff47b5f2b5bf972e6d92219f95b
Diogo Behrens <diogo.behrens@huawei.com>
2026-02-02 18:52:28 +0000

    refactor: use multiple modules slots for runtime
    Signed-off-by: Diogo Behrens <diogo.behrens@huawei.com>

--- modules/atomic/src/flags.c
+++ modules/atomic/src/flags.c
@@ -1,7 +1,7 @@
 #define LOGGER_PREFIX LOGGER_CUR_FILE
 #define LOGGER_BLOCK  LOGGER_CUR_BLOCK
 #include <lotto/cli/flagmgr.h>
-#include <lotto/states/handlers/atomic.h>
+#include <lotto/modules/atomic/state.h>
 
 NEW_PRETTY_CALLBACK_FLAG(HANDLER_ATOMIC_ENABLED, "", "handler-atomic",
                          "enable atomic handler", flag_on(), STR_CONVERTER_BOOL,

02c9960d8438f21ba435191189a75dbed773c858
Diogo Behrens <diogo.behrens@huawei.com>
2026-02-02 12:56:00 +0000

    fix: convert filters and race/ichpt to dice-module
    Signed-off-by: Diogo Behrens <diogo.behrens@huawei.com>

--- modules/race/src/state.c
+++ modules/race/src/state.c
@@ -1,9 +1,7 @@
-/*
- */
 #define LOGGER_PREFIX LOGGER_CUR_FILE
 #define LOGGER_BLOCK  LOGGER_CUR_BLOCK
 #include <lotto/brokers/statemgr.h>
-#include <lotto/states/handlers/race.h>
+#include <lotto/modules/race/state.h>
 #include <lotto/sys/logger_block.h>
 
 static race_config_t _config;
@@ -11,11 +9,11 @@ static race_config_t _config;
 REGISTER_CONFIG(_config, {
     logger_infof("enabled            = %s\n", _config.enabled ? "on" : "off");
     logger_infof("ignore_write_write = %s\n",
-              _config.ignore_write_write ? "on" : "off");
+                 _config.ignore_write_write ? "on" : "off");
     logger_infof("only_write_ichpt   = %s\n",
-              _config.only_write_ichpt ? "on" : "off");
+                 _config.only_write_ichpt ? "on" : "off");
     logger_infof("abort_on_race      = %s\n",
-              _config.abort_on_race ? "on" : "off");
+                 _config.abort_on_race ? "on" : "off");
 })
 
 race_config_t *

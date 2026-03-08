ae0b241c5b4b323162d52d8848dc2d859072fb04
Diogo Behrens <db7@sdf.org>
2026-03-08 22:38:44 +0100

    refactor: rename plugins as modules to match structure
    Signed-off-by: Diogo Behrens <db7@sdf.org>

--- src/include/lotto/sys/modules.h
+++ src/include/lotto/sys/modules.h
@@ -4,49 +4,49 @@
 #include <stdbool.h>
 
 typedef enum {
-    PLUGIN_KIND_NONE    = 0,
-    PLUGIN_KIND_CLI     = 1,
-    PLUGIN_KIND_ENGINE  = 1 << 1,
-    PLUGIN_KIND_RUNTIME = 1 << 2,
-    PLUGIN_KIND_MEMMGR  = 1 << 3,
-} plugin_kind_t;
+    MODULE_KIND_NONE    = 0,
+    MODULE_KIND_CLI     = 1,
+    MODULE_KIND_ENGINE  = 1 << 1,
+    MODULE_KIND_RUNTIME = 1 << 2,
+    MODULE_KIND_MEMMGR  = 1 << 3,
+} module_kind_t;
 
-#define PLUGIN_ALL (~0U)
+#define MODULE_ALL (~0U)
 
-typedef struct plugin_s {
-    char *name; //< plugin name, e.g. 'priority'
-    char *path; //< plugin path, e.g. '/PATH/TO/liblotto_priority_engine.so'
-    plugin_kind_t kind; //< plugin kind
-    bool shadowed;      //< whether it is shadowed by another plugin lib file
-    bool disabled; //< disabled by explicit user plugin selection, or failed to
+typedef struct module_s {
+    char *name; //< module name, e.g. 'priority'
+    char *path; //< module path, e.g. '/PATH/TO/liblotto_priority_engine.so'
+    module_kind_t kind; //< module kind
+    bool shadowed;      //< whether it is shadowed by another module lib file
+    bool disabled; //< disabled by explicit user module selection, or failed to
                    // load
-} plugin_t;
+} module_t;
 
-typedef int (*plugin_foreach_f)(plugin_t *, void *);
+typedef int (*module_foreach_f)(module_t *, void *);
 
-/** Scan the plugin dirs and populate the plugin registry. */
-void lotto_plugin_scan(const char *build_dir, const char *install_dir,
-                       const char *plugin_dir);
+/** Scan the module dirs and populate the module registry. */
+void lotto_module_scan(const char *build_dir, const char *install_dir,
+                       const char *module_dir);
 
-/** Disables all but the comma seperated list of plugins. Does nothing if NULL
+/** Disables all but the comma seperated list of modules. Does nothing if NULL
  * is passed. */
-int lotto_plugin_enable_only(const char *plugin_list);
+int lotto_module_enable_only(const char *module_list);
 
-/** Clear the plugin registry. */
-void lotto_plugin_clear();
+/** Clear the module registry. */
+void lotto_module_clear();
 
-/** Print the plugin registry to stdout. */
-void lotto_plugin_print();
-const char *lotto_plugin_kind_str(plugin_kind_t kind);
+/** Print the module registry to stdout. */
+void lotto_module_print();
+const char *lotto_module_kind_str(module_kind_t kind);
 
-/** Do something on each plugin.
+/** Do something on each module.
  * If some f returns a non-zero value, the function returns immediately that
  * value. If all f's return zero, the function returns zero.
  */
-int lotto_plugin_foreach(plugin_foreach_f f, void *arg);
-int lotto_plugin_foreach_all(plugin_foreach_f f, void *arg);
+int lotto_module_foreach(module_foreach_f f, void *arg);
+int lotto_module_foreach_all(module_foreach_f f, void *arg);
 
-/** Similar to lotto_plugin_foreach but in the reverse order. */
-int lotto_plugin_foreach_reverse(plugin_foreach_f f, void *arg);
+/** Similar to lotto_module_foreach but in the reverse order. */
+int lotto_module_foreach_reverse(module_foreach_f f, void *arg);
 
 #endif

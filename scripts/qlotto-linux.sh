c008a92a9987e0c77d303309b5db6e9269ea06a1
Martin Beck <martin.beck2@gmx.de>
2026-03-09 10:50:14 +0100

    fix: path to lotto qemu plugins and qemu 9.1.3 patch
    Fixes qemu 9.1.3 patch to match new directory structure for qlotto
    header files
    
    Signed-off-by: Martin Beck <martin.beck2@gmx.de>
    Signed-off-by: Diogo Behrens <db7@sdf.org>

--- scripts/qlotto-linux.sh
+++ scripts/qlotto-linux.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+ : ${PLUGIN_PATH:="build/qlotto/modules"}
+
 LINUX_VERSION=6.13.4
 MEM=200M
 
@@ -11,8 +13,8 @@ exec ${QEMU_CMD} \
     -machine virt -m ${MEM} -cpu cortex-a57 -nographic -smp 2 -no-reboot \
     -kernel demos/linux/linux-${LINUX_VERSION}/arch/arm64/boot/Image \
     -initrd demos/linux/rootfs.img \
-    -plugin build/qlotto/plugins/libplugin-lotto.so \
-    -plugin build/qlotto/plugins/libplugin-gdb-server.so \
+    -plugin ${PLUGIN_PATH}/libplugin-lotto.so \
+    -plugin ${PLUGIN_PATH}/libplugin-gdb-server.so \
     -d plugin \
     -append "init=/init.sh serial=ttyAMA0 root=/dev/ram panic=-1 -- $*" 2>&1
 

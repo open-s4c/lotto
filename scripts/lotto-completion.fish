69a1ef0c5ef4dfa14f657dedb5bb1e254a6fc88a
Diogo Behrens <db7@sdf.org>
2026-03-10 08:25:52 +0100

    feat: cache for autocompletion
    Signed-off-by: Diogo Behrens <db7@sdf.org>

--- scripts/lotto-completion.fish
+++ scripts/lotto-completion.fish
@@ -3,6 +3,70 @@
 # This completion queries commands and flags at runtime:
 #   - commands: `lotto --list-commands`
 #   - flags:    `lotto [<cmd>] --list-flags`
+# and caches results briefly for responsiveness.
+
+function __fish_lotto_cache_key
+    string replace -ar '[^A-Za-z0-9_.-]' '_' -- $argv[1]
+end
+
+function __fish_lotto_bin_mtime
+    set -l bin $argv[1]
+    if test -x "$bin"
+        command stat -c %Y "$bin" 2>/dev/null
+        return
+    end
+    if command -sq $bin
+        set -l resolved (command -s $bin)
+        command stat -c %Y "$resolved" 2>/dev/null
+        return
+    end
+    echo 0
+end
+
+function __fish_lotto_cache_get
+    set -l cache_id $argv[1]
+    set -l bin $argv[2]
+    set -e argv[1..2]
+
+    set -l ttl 3
+    if set -q LOTTO_COMPLETION_CACHE_TTL
+        set ttl $LOTTO_COMPLETION_CACHE_TTL
+    end
+
+    set -l root $HOME/.cache/lotto-completion
+    if set -q XDG_CACHE_HOME
+        set root $XDG_CACHE_HOME/lotto-completion
+    end
+    if not command mkdir -p $root 2>/dev/null
+        set root /tmp/lotto-completion
+        command mkdir -p $root 2>/dev/null; or return
+    end
+
+    set -l file "$root/"(__fish_lotto_cache_key "$cache_id")
+    set -l mtime (__fish_lotto_bin_mtime $bin)
+    set -l now (date +%s)
+
+    if test -r $file
+        set -l hdr (command head -n 2 $file)
+        set -l cached_mtime $hdr[1]
+        set -l cached_ts $hdr[2]
+        if test "$cached_mtime" = "$mtime"
+            set -l age (math "$now - $cached_ts" 2>/dev/null)
+            if test -n "$age"; and test $age -lt $ttl
+                command tail -n +3 $file
+                return
+            end
+        end
+    end
+
+    set -l out (command $argv 2>/dev/null)
+    begin
+        echo $mtime
+        echo $now
+        printf "%s\n" $out
+    end >$file
+    printf "%s\n" $out
+end
 
 function __fish_lotto_bin
     set -l tokens (commandline -opc)
@@ -26,7 +90,7 @@ end
 
 function __fish_lotto_commands_raw
     set -l bin (__fish_lotto_bin); or return
-    command $bin --list-commands 2>/dev/null
+    __fish_lotto_cache_get "$bin|commands" $bin $bin --list-commands
 end
 
 function __fish_lotto_commands
@@ -57,9 +121,9 @@ function __fish_lotto_flags_raw
     set -l bin (__fish_lotto_bin); or return
     set -l subcmd (__fish_lotto_selected_subcmd)
     if test -n "$subcmd"
-        command $bin $subcmd --list-flags 2>/dev/null
+        __fish_lotto_cache_get "$bin|flags|$subcmd" $bin $bin $subcmd --list-flags
     else
-        command $bin --list-flags 2>/dev/null
+        __fish_lotto_cache_get "$bin|flags|root" $bin $bin --list-flags
     end
 end
 

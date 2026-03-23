8d8c1338d5bb2e7c8b06cb678208554901ffecf3
Diogo Behrens <db7@sdf.org>
2026-03-23 15:37:57 +0100

    fix: advertise event type before macro expansion
    Signed-off-by: Diogo Behrens <db7@sdf.org>

--- modules/mutex/src/flags.c
+++ modules/mutex/src/flags.c
@@ -1,6 +1,12 @@
 #define LOGGER_BLOCK LOGGER_CUR_BLOCK
 #include "state.h"
+#include <lotto/engine/pubsub.h>
 #include <lotto/driver/flagmgr.h>
+#include <lotto/modules/mutex/events.h>
+
+LOTTO_ADVERTISE_TYPE(EVENT_MUTEX_ACQUIRE)
+LOTTO_ADVERTISE_TYPE(EVENT_MUTEX_TRYACQUIRE)
+LOTTO_ADVERTISE_TYPE(EVENT_MUTEX_RELEASE)
 
 NEW_PRETTY_CALLBACK_FLAG(HANDLER_MUTEX_ENABLED, "", "handler-mutex",
                          "enable mutex handler", flag_on(), STR_CONVERTER_BOOL,

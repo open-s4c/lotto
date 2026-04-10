c3c806a445819b802698008c615d8ea42395707f
Diogo Behrens <db7@sdf.org>
2026-04-10 15:05:25 +0200

    feat: add interface to driver/exec.c to accept a prefix
    Signed-off-by: Diogo Behrens <db7@sdf.org>

--- include/lotto/driver/events.h
+++ include/lotto/driver/events.h
@@ -5,8 +5,11 @@
 #ifndef LOTTO_CORE_DRIVER_EVENTS_H
 #define LOTTO_CORE_DRIVER_EVENTS_H
 
-#define EVENT_DRIVER__MEMMGR_INIT       133
-#define EVENT_DRIVER__REGISTER_FLAGS    134
+/** Driver event: initialize memory-manager-related state. */
+#define EVENT_DRIVER__MEMMGR_INIT 133
+/** Driver event: register CLI flags contributed by modules. */
+#define EVENT_DRIVER__REGISTER_FLAGS 134
+/** Driver event: register CLI subcommands contributed by modules. */
 #define EVENT_DRIVER__REGISTER_COMMANDS 135
 
 #endif

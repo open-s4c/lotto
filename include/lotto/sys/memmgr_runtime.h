7b4e5476de565d6334086bc19d2a9007e9c1e7e9
Diogo Behrens <db7@sdf.org>
2026-04-17 08:25:38 +0200

    fix: remove dangling .so files after disabling module
    Signed-off-by: Diogo Behrens <db7@sdf.org>

--- include/lotto/sys/memmgr_runtime.h
+++ include/lotto/sys/memmgr_runtime.h
@@ -9,10 +9,10 @@
 
 #include <lotto/util/macros.h>
 
-void *memmgr_runtime_alloc(size_t size);
+void *memmgr_runtime_alloc(size_t size) WEAK;
 void *memmgr_runtime_aligned_alloc(size_t alignment, size_t size) WEAK;
 void *memmgr_runtime_realloc(void *ptr, size_t size) WEAK;
-void memmgr_runtime_free(void *ptr);
+void memmgr_runtime_free(void *ptr) WEAK;
 void memmgr_runtime_fini() WEAK;
 
 #endif

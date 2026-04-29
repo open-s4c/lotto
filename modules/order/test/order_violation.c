33a962bcefa3104f51657ab05dafbe36f2691e82
Diogo Behrens <db7@sdf.org>
2026-04-29 21:11:55 +0200

    feat: use in-tree deadline helper for tikl timeouts and similar
    Signed-off-by: Diogo Behrens <db7@sdf.org>

--- modules/order/test/order_violation.c
+++ modules/order/test/order_violation.c
@@ -1,5 +1,5 @@
 // clang-format off
-// RUN: !( timeout -k 5s 4s %ntlotto %stress -r 1 -- %b)
+// RUN: !( %deadline 4s %ntlotto %stress -r 1 -- %b)
 // clang-format on
 
 #include <lotto/order.h>
@@ -10,4 +10,3 @@ main()
     lotto_order(2);
     return 0;
 }
-

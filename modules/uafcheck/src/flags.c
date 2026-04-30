ec3f8bd07f0908bdd5c0fbc14991aa4a5444c6f2
Diogo Behrens <db7@sdf.org>
2026-04-30 07:23:35 +0200

    uafcheck: report invalid probability range
    Signed-off-by: Diogo Behrens <db7@sdf.org>

--- modules/uafcheck/src/flags.c
+++ modules/uafcheck/src/flags.c
@@ -1,6 +1,8 @@
 #include "state.h"
 #include <lotto/driver/flagmgr.h>
 #include <lotto/sys/assert.h>
+#include <lotto/sys/stdio.h>
+#include <lotto/sys/stdlib.h>
 
 NEW_CALLBACK_FLAG(UAFCHECK_MAX_PAGES, "", "uafcheck-max-pages", "INT",
                   "number of freed pages kept protected by uafcheck",
@@ -12,6 +14,13 @@ NEW_CALLBACK_FLAG(UAFCHECK_MAX_PAGES, "", "uafcheck-max-pages", "INT",
 NEW_CALLBACK_FLAG(UAFCHECK_PROB, "", "uafcheck-prob", "FLOAT",
                   "probability that an allocation is protected by uafcheck",
                   flag_dval(UAFCHECK_DEFAULT_PROB), {
-                      ASSERT(as_dval(v) >= 0.0 && as_dval(v) <= 1.0);
+                      double prob = as_dval(v);
+                      if (prob < 0.0 || prob > 1.0) {
+                          sys_fprintf(stderr,
+                                      "error: --uafcheck-prob must be in range "
+                                      "[0, 1]: %g\n",
+                                      prob);
+                          sys_exit(1);
+                      }
                       uafcheck_config()->prob = as_dval(v);
                   })

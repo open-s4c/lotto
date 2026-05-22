7998d798b67f9051c0a648cc2fac196ad2c00888
Martin Beck <martin.beck2@gmx.de>
2026-05-22 15:51:51 +0200

    cleanup after testing
    Signed-off-by: Martin Beck <martin.beck2@gmx.de>

--- modules/qemu/src/emit_loop.c
+++ modules/qemu/src/emit_loop.c
@@ -1,22 +1,21 @@
 #include "interceptors.h"
 #include <lotto/modules/qemu/util.h>
 #include <lotto/yield.h>
-#include <stdio.h>
 
 uint64_t branch_back_count = 0;
 
 void
 emit_loop(unsigned int cpu_index, void *udata)
 {
-    uint64_t reduction = 1000; // 100000
+    uint64_t reduction = 1000;
 
     (void)udata;
     if (!qemu_instrumentation_enabled(cpu_index)) {
         return;
     }
 
+    branch_back_count++;
     if (branch_back_count % reduction == 0) {
         lotto_yield(true);
     }
-    branch_back_count++;
 }
